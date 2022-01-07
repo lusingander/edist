9f771a6d0d52995ef1aa1bf0c81994511450131c
Kyosuke Fujimoto <kyoro.f@gmail.com>
2022-01-07 18:50:03 +0900

    Use bubbletea

--- main.go
+++ main.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"io/fs"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type sticky struct {
 	path     string
 	name     string
 	contents []fs.DirEntry
+}
+
+func (s *sticky) Title() string {
+	return s.name
+}
+
+func (s *sticky) Description() string {
+	return s.path
+}
+
+func (s *sticky) FilterValue() string {
+	return s.name
 }
 
 func stickiesDataDir() (string, error) {
@@ -88,6 +103,36 @@ func checkOS() error {
 	return nil
 }
 
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type model struct {
+	list list.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" {
+			return m, nil
+		}
+	case tea.WindowSizeMsg:
+		top, right, bottom, left := docStyle.GetMargin()
+		m.list.SetSize(msg.Width-left-right, msg.Height-top-bottom)
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return docStyle.Render(m.list.View())
+}
+
 func run(args []string) error {
 	if err := checkOS(); err != nil {
 		return err
@@ -96,10 +141,17 @@ func run(args []string) error {
 	if err != nil {
 		return err
 	}
-	for _, sticky := range stickies {
-		fmt.Println(sticky.path)
+
+	items := make([]list.Item, len(stickies))
+	for i, s := range stickies {
+		items[i] = s
 	}
-	return err
+	m := model{list: list.NewModel(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "EDIST"
+
+	p := tea.NewProgram(m)
+	p.EnterAltScreen()
+	return p.Start()
 }
 
 func main() {
